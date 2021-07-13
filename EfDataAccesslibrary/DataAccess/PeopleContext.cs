e45339a3762c9ff70be061ee759f2cee54336cbd
kayhannkaya <kayhankaya34@gmail.com>
2021-07-13 14:07:46 +0300

    Add project files.

---
+++ EfDataAccesslibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,21 @@
+﻿using EfDataAccesslibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfDataAccesslibrary.DataAccess
+{
+    //Conrol + . for comes features C#
+  public class PeopleContext:DbContext
+    {
+        public PeopleContext(DbContextOptions options):base(options)
+        {
+
+        }
+        //These are Set the data
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
